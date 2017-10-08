6968cd9ff2cc7d8f0dc6ccfc578ff05c99db7fa8
christeensj <christeensj5@gmail.com>
2017-10-08 05:30:40 +0000

    Abc

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -2,4 +2,10 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope, $sce) {
+$scope.messages=[
+{ 'sender':'USER','text':'hi','time':'10:01 AM'},
+{ 'sender':'BOT','text':'What can i do for you?','time':'10:03 AM'},
+{ 'sender':'USER','text':'News about North Korea','time':'10:03 AM'}
+]
+
 });
